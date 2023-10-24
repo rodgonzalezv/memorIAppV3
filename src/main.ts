dd0a644f405b81683776a08e8af18e0b7c4a9ef1
bpina0506 <bladimir48@gmail.com>
2023-10-24 19:57:32 -0300

    camera fixed with PWA v2

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,6 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import { defineCustomElements } from '@ionic/pwa-elements/loader';
 
-if (environment.production) {
-  enableProdMode();
-}
-
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
 
