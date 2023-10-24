445fa70ebd58788f89300ba9cd5d7e9d02cfe2ed
Rodrigo Gonzáledz <rod.gonzalezv@duocuc.cl>
2023-10-24 14:08:29 -0300

    coordenadas de gps listas.

--- src/app/gps/gps.page.ts
+++ src/app/gps/gps.page.ts
@@ -6,29 +6,26 @@ import { Geolocation } from '@capacitor/geolocation';
   templateUrl: './gps.page.html',
   styleUrls: ['./gps.page.scss'],
 })
-
-
-
 export class GpsPage implements OnInit {
+
+  coordinates: string = 'Esperando coordenadas...';
 
   constructor() { }
 
   ngOnInit() {
-
-    const printCurrentPosition = async () => {
-      const coordinates = await Geolocation.getCurrentPosition();
-    
-      console.log('Current position:', coordinates);
-    };
-
+    this.printCurrentPosition();
   }
 
-  
-
-
+  async printCurrentPosition() {
+    try {
+      const position = await Geolocation.getCurrentPosition();
+      const lat = position.coords.latitude;
+      const lon = position.coords.longitude;
+      this.coordinates = `Latitud: ${lat}, Longitud: ${lon}`;
+      console.log('Coordenadas actuales:', position);
+    } catch (error) {
+      console.error('Error al obtener las coordenadas:', error);
+      this.coordinates = 'Error al obtener las coordenadas';
+    }
+  }
 }
-
-
-
-
-
